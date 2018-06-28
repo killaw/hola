fb2ac963ee032f44907cddf5d5270e6700ed5987
killaw <nd.nikitin@gmail.com>
2018-06-28 23:41:26 +0300

    Replace fs.stat file exists checking with flags in WriteStream

--- post.js
+++ post.js
@@ -8,65 +8,57 @@ const post = function (filename, req, res) {
     return;
   }
 
-  fs.stat(filename, (err) => {
-    if (err && err.code !== 'ENOENT') {
-      res.statusCode = 500;
-      res.end('Server error');
+  let stream = new fs.WriteStream(filename, {flags: 'wx'});
+  let length = 0;
 
-      return;
-    } else if (!err) {
+  const writeFile = function (chunk) {
+    length += chunk.length;
+    if (length >= 1048576) {
+      console.log(length);
+      stream.once('close', () => {
+        fs.unlink(filename, (err) => {
+          if (err)
+            console.error(err);
+        });
+      });
+      req.unpipe(stream);
+      stream.destroy();
+      res.statusCode = 413;
+      res.end('File is too big');
+      req.removeListener('data', writeFile);
+    }
+  };
+
+  stream.on('error', (err) => {
+    if (err.code === 'EEXIST') {
       res.statusCode = 409;
       res.end('Alredy exists');
-
-      return;
-    }
-
-    let stream = new fs.WriteStream(filename);
-    let length = 0;
-
-    const writeFile = function (chunk) {
-      length += chunk.length;
-      if (length >= 1048576) {
-        console.log(length);
-        stream.once('close', () => {
-          fs.unlink(filename, (err) => {
-            if (err)
-              console.error(err);
-          });
-        });
-        req.unpipe(stream);
-        stream.destroy();
-        res.statusCode = 413;
-        res.end('File is too big');
-        req.removeListener('data', writeFile);
-      }
-    };
-
-    stream.on('error', (err) => {
+    } else {
       res.statusCode = 500;
       res.end('Server error');
-      console.error(err);
-    });
+    }
 
-    req.pipe(stream);
-
-    req
-      .on('data', writeFile)
-      .on('close', () => {
-        stream.once('close', () => {
-          fs.unlink(filename, (err) => {
-            if (err)
-              console.error(err);
-          });
-        });
-        req.unpipe(stream);
-        stream.destroy();
-      })
-      .on('end', () => {
-        res.statusCode = 200;
-        res.end('Upload completed');
-      });
+    console.error(err);
   });
+
+  req.pipe(stream);
+
+  req
+    .on('data', writeFile)
+    .on('close', () => {
+      stream.once('close', () => {
+        fs.unlink(filename, (err) => {
+          if (err)
+            console.error(err);
+        });
+      });
+      req.unpipe(stream);
+      stream.destroy();
+    })
+    .on('end', () => {
+      res.statusCode = 200;
+      res.end('Upload completed');
+    });
 };
 
 module.exports = post;
