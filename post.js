49ee3e735e318b2de65e2d1411cbbad98a42d297
killaw <nd.nikitin@gmail.com>
2018-06-29 15:35:55 +0300

    Add Connection close header to end request correctly

--- post.js
+++ post.js
@@ -1,7 +1,7 @@
 let fs = require('fs');
 
 const post = function (filename, req, res) {
-  if (req.headers['content-length'] >= 1048576) {
+  if (+req.headers['content-length'] >= 1048576) {
     res.statusCode = 413;
     res.end('File too big');
 
@@ -14,18 +14,35 @@ const post = function (filename, req, res) {
   const writeFile = function (chunk) {
     length += chunk.length;
     if (length >= 1048576) {
-      console.log(length);
-      stream.once('close', () => {
+      /*
+        Обязательно нужно отправлять заголовок Connection:close, т.к. браузеры,
+        не смотря на то, что мы делаем res.end(), всё равно продолжат отдавать
+        данные поддерживая соединение
+        // if we just res.end w/o connection close, browser may keep on sending the file
+        // the connection will be kept alive, and the browser will hang (trying to send more data)
+        // this header tells node to close the connection
+        // also see http://stackoverflow.com/questions/18367824/how-to-cancel-http-upload-from-data-events/18370751#18370751
+      */
+      res.setHeader('Connection', 'close');
+      res.statusCode = 413;
+      res.end('File is too big');
+      // req.removeListener('data', writeFile);
+
+      /*
+        unpipe делает хер пойми что. Т.е. поток не будет приостановлен, данные
+        можут потеряться неизвестно где. (Так исторически сложилось)
+        Если хочется при пайпе поставить поток на паузу, то нужно делать pause(),
+        а потом resume()
+        req.unpipe(stream);
+      */
+      stream.destroy();
+
+      // stream.once('close', () => {
         fs.unlink(filename, (err) => {
           if (err)
             console.error(err);
         });
-      });
-      req.unpipe(stream);
-      stream.destroy();
-      res.statusCode = 413;
-      res.end('File is too big');
-      req.removeListener('data', writeFile);
+      // });
     }
   };
 
@@ -41,24 +58,24 @@ const post = function (filename, req, res) {
     console.error(err);
   });
 
-  req.pipe(stream);
 
   req
     .on('data', writeFile)
     .on('close', () => {
-      stream.once('close', () => {
+      // req.unpipe(stream);
+      stream.destroy();
+      // stream.once('close', () => {
         fs.unlink(filename, (err) => {
           if (err)
             console.error(err);
         });
-      });
-      req.unpipe(stream);
-      stream.destroy();
+      // });
     })
     .on('end', () => {
       res.statusCode = 200;
       res.end('Upload completed');
-    });
+    })
+    .pipe(stream); // здесь уже вернется WriteStream, т.е. не объект req
 };
 
 module.exports = post;
