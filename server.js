bb2346cc9e4204a791a783719429a2f5a7018ea4
killaw <nd.nikitin@gmail.com>
2018-07-12 14:53:57 +0300

    Some ciphers settings #7

--- server.js
+++ server.js
@@ -17,14 +17,23 @@ let httpsOptions = {
     'ECDHE-ECDSA-AES128-GCM-SHA256',
     'ECDHE-RSA-AES256-GCM-SHA384',
     'ECDHE-ECDSA-AES256-GCM-SHA384',
-    'ECDHE-RSA-AES128-SHA256',
-    'ECDHE-RSA-AES256-SHA384',
-    'ECDHE-RSA-AES256-SHA256',
     'DHE-RSA-AES128-GCM-SHA256',
+    'ECDHE-RSA-AES128-SHA256',
     'DHE-RSA-AES128-SHA256',
+    'ECDHE-RSA-AES256-SHA384',
     'DHE-RSA-AES256-SHA384',
+    'ECDHE-RSA-AES256-SHA256',
     'DHE-RSA-AES256-SHA256',
-    'HIGH'
+    'HIGH',
+    '!aNULL',
+    '!eNULL',
+    '!EXPORT',
+    '!DES',
+    '!RC4',
+    '!MD5',
+    '!PSK',
+    '!SRP',
+    '!CAMELLIA'
   ].join(':'),
   honorCipherOrder: true
 };
