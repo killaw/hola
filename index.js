d2e6551b3f8463fef392acf670822afe222049e2
killaw <nd.nikitin@gmail.com>
2018-07-13 18:29:57 +0300

    Change app ports

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const server = require('./server'),
       config = require('./config/index'),
       httpServer = require('redirect-to-https');
 
-httpServer.listen(80);
+httpServer.listen(8080);
 server.listen(config.get('app:port'));
